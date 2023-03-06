7d3760393b980c28c15a4fc38b2718b66d7fb54e
liwens6 <lwq200402@163.com>
2023-03-06 19:07:02 +0800

    solved

--- tk.py
+++ tk.py
@@ -1,10 +1,22 @@
 import tkinter as tk
-
 window = tk.Tk()
-window.title('helloworld')
-window.geometry('400x400')
-
-l = tk.Label(window, text='fuck u', bg="green",font=("Arial",12),width = 15,height = 2)
+window.title('my first window')
+window.geometry('200x100')
+var = tk.StringVar()
+l = tk.Label(window,textvariable=var,bg = 'green',font=('Arial',12),width = 25,height=4)
 l.pack()
+is_Hidden = False
+def hidden_me():
+    global is_Hidden
+    if is_Hidden ==False:
+        is_Hidden = True
+        var.set('clivk me and hidden')
+    else:
+        is_Hidden = False
+        var.set('')
 
-window.mainloop
+var1 = tk.StringVar()
+var1.set('hidden')
+b = tk.Button(window,textvariable=var1,width=12,height=2,command=hidden_me)
+b.pack()
+window.mainloop()
