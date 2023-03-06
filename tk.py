1a30601e0af97e272c3a94c81c72f5e1cf1b9d80
liwens6 <lwq200402@163.com>
2023-03-06 12:20:56 +0800

    first change

---
+++ tk.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+windows = tk.Tk()
+windows.title("helloworld")
+windows.geometry("400x400")
+l = tk.Label(windows, text="hello world", bg="green",font=("Arial",12),width = 15,height = 2)
+l.pack()
+windows.mainloop
